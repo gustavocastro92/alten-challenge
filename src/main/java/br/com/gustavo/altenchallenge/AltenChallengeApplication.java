14ab2798a727b6686a98cc59c68618f305b3a94e
Gustavo Castro <gustavogcastro1992@gmail.com>
2021-09-18 10:10:03 -0300

    initial commit

---
+++ src/main/java/br/com/gustavo/altenchallenge/AltenChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.altenchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltenChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltenChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/altenchallenge/AltenChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.altenchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltenChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
